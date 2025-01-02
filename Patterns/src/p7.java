5022589513ca96ccd9cd4c624f602a795b1ddfb5
Pallab Rajbongshi <pallabops07@gmail.com>
2025-01-02 22:04:09 +0530

    p7.java

--- Patterns/src/p7.java
+++ Patterns/src/p7.java
@@ -1,5 +1,27 @@
+
+import java.util.Scanner;
+
 public class p7 {
     public static void main(String[] args) {
-        System.out.println("hello");
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("enter the value of n : ");
+        int n = in.nextInt();
+
+        for(int i =0; i<n; i++){
+
+            for(int j =0; j<n-i-1; j++){
+                System.out.print(" ");
+            }
+            for(int k =0; k<2*i+1; k++){
+                System.out.print("*");
+            }
+            for(int j =0; j<n-i-1; j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+            in.close();
+        }
+
     }
 }
